15bae8009bb747c1c82f672a23545b74dfbac66f
dev-guidolin <developer.arruda@gmail.com>
2022-04-23 11:22:04 -0300

    :heavy_plus_sign: feact: Adicionar templates
    Adicionar templates para as views do projeto.

---
+++ resources/views/pages/contact_list.blade.php
@@ -0,0 +1,19 @@
+@extends('template.app')
+@section('title')
+    Agenda Alf Soft
+@endSection()
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 mx-auto">
+                <div class="card">
+                    <div class="car-body">
+                        <h3 class="card-title">Aqui é um card</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('scripts')
+@endSection()
---
+++ resources/views/template/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> @yield('title') </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    @yield('exstra-css')
+    @yield('exstra-style')
+</head>
+<body>
+
+@yield('content')
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+@yield('scripts')
+@yield('exstra-js')
+</body>
+</html>
